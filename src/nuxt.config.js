fd5984505ca4e96f1a6817e094fb4cdd7a770d64
albasimia <albasimia888@gmail.com>
2018-08-03 01:47:04 +0900

    flamelink設定完了

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -20,20 +20,35 @@ module.exports = {
   /*
   ** Build configuration
   */
-  build: {
-    /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
-  }
+ buildDir: '../functions/nuxt',
+ build: {
+   publicPath: '/assets/',
+   extractCSS: true,
+   babel: {
+     presets: [
+       'env',
+       'stage-0'
+     ],
+     plugins: [
+       ['transform-runtime', {
+         polyfill: true,
+         regenerator: true
+       }],
+     ],
+   },
+   /*
+   ** Run ESLint on save
+   */
+   extend (config, { isDev, isClient }) {
+     if (isDev && isClient) {
+       config.module.rules.push({
+         enforce: 'pre',
+         test: /\.(js|vue)$/,
+         loader: 'eslint-loader',
+         exclude: /(node_modules)/
+       })
+     }
+   }
+ }
 }
 
